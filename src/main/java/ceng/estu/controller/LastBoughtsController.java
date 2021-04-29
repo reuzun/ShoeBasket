74a387a09242ea443134f68e600fb73f6eff0c34
reuzun <moruka12@yandex.com>
2021-04-29 17:11:30 +0300

    last bought page initialized

---
+++ src/main/java/ceng/estu/controller/LastBoughtsController.java
@@ -0,0 +1,12 @@
+package ceng.estu.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
+
+/**
+ * @author reuzun
+ */
+public class LastBoughtsController {
+
+}
